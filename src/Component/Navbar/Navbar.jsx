33c781fc6773a1f4e30b2753ab4fba298a9ca185
Sadiqur199 <sadiqurrhaman199@gmail.com>
2024-04-21 17:10:06 +0600

    navbar design complete

--- src/Component/Navbar/Navbar.jsx
+++ src/Component/Navbar/Navbar.jsx
@@ -1,22 +1,20 @@
-import React from 'react';
-import './Navbar.css'
+import React from "react";
+import "./Navbar.css";
 import { FaTruckMoving } from "react-icons/fa";
-import logo from '../../../public/image/h-removebg-preview.png';
+import logo from "../../../public/image/h-removebg-preview.png";
 import { CiHeart } from "react-icons/ci";
 import { MdOutlineShoppingBag } from "react-icons/md";
 import { FaUserCircle } from "react-icons/fa";
-
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
     <div>
-      <div className='nav-header'>
-          <div className='icon'>
-            <FaTruckMoving/>
-          </div>
-          <p>
-          FREE shipping when shopping upto $1000
-        </p>
+      <div className="nav-header">
+        <div className="icon">
+          <FaTruckMoving />
+        </div>
+        <p>FREE shipping when shopping upto $1000</p>
       </div>
       <div className="main_header">
         <div className="container">
@@ -24,18 +22,50 @@ const Navbar = () => {
             <img className="logo" src={logo} alt="" />
           </div>
           <div className="search_box">
-            <input type="search" name="" id="" placeholder='Enter Your Product Name' />
+            <input
+              type="search"
+              name=""
+              id=""
+              placeholder="Enter Your Product Name"
+            />
             <button>Search</button>
           </div>
           <div className="icon">
             <div className="account">
               <div className="user_icon">
-              <FaUserCircle />
+                <FaUserCircle />
               </div>
               <p>Hello ,user</p>
             </div>
-          <CiHeart />
-          <MdOutlineShoppingBag />
+            <div className="second_icon">
+              <Link to="/" className="link">
+                <CiHeart />
+              </Link>
+              <Link to="/cart" className="link">
+                <MdOutlineShoppingBag />
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="header">
+        <div className="container">
+          <ul>
+            <li>
+              <Link to='/'>Home</Link>
+            </li>
+            <li>
+              <Link to='/product'>Product</Link>
+            </li>
+            <li>
+              <Link to='/about'>About</Link>
+            </li>
+            <li>
+              <Link to='/contact'>Contact</Link>
+            </li>
+          </ul>
+          <div className="auth">
+
           </div>
         </div>
       </div>
