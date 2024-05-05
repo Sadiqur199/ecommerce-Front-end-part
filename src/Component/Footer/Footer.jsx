174caa471bb3c97c2f5f7d658b6c7b4f201d18c4
Sadiqur199 <sadiqurrhaman199@gmail.com>
2024-05-06 04:11:56 +0600

    design footer section

--- src/Component/Footer/Footer.jsx
+++ src/Component/Footer/Footer.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
 import './Footer.css'
+import logo from "../../../public/image/h-removebg-preview.png";
 const Footer = () => {
   return (
     <div>
-      
+      <div className="footer">
+        <div className="container">
+          <div className="about">
+            <div className="logo">
+              <img src={logo} alt="logo" />
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
-
 export default Footer;
