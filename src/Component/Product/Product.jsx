2b77b84cc688342d3d964451733d763fc440510c
Sadiqur199 <sadiqurrhaman199@gmail.com>
2024-05-17 18:16:01 +0600

    update product page

--- src/Component/Product/Product.jsx
+++ src/Component/Product/Product.jsx
@@ -1,9 +1,61 @@
-import React from 'react';
-import './Product.css'
+import React from "react";
+import "./Product.css";
+import ProductApi from "../ProductApi/ProductApi";
+import { IoCartOutline, IoEyeOutline } from "react-icons/io5";
+import { FaRegHeart } from "react-icons/fa";
 const Product = () => {
   return (
     <div>
-      <h1>Product</h1>
+      <h2># Products</h2>
+      <p>Home.Products</p>
+      <div className="products">
+        <div className="container">
+          <div className="filter">
+            <div className="categories">
+              <h3>Categories</h3>
+              <ul>
+                <li>Man</li>
+                <li>Woman</li>
+                <li>Children</li>
+              </ul>
+            </div>
+          </div>
+          <div className="productbox">
+            <div className="container">
+              {ProductApi.map((curElm) => {
+                return (
+                  <>
+                    <div className="box" key={curElm.id}>
+                      <div className="img_box">
+                        <img src={curElm.Img} alt={curElm.Title} />
+                        <div className="icon">
+                          <li>
+                            {" "}
+                            <IoCartOutline />
+                          </li>
+                          <li>
+                            {" "}
+                            <IoEyeOutline/>
+                          </li>
+                          <li>
+                            {" "}
+                            <FaRegHeart />
+                          </li>
+                        </div>
+                      </div>
+                      <div className="detail">
+                        <p>{curElm.cat}</p>
+                        <h3>{curElm.Title}</h3>
+                        <h4>{curElm.price}</h4>
+                      </div>
+                    </div>
+                  </>
+                );
+              })}
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
