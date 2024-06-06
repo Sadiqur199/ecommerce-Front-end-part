494002174456029a79866a92eb6d5da82dc1c278
Sadiqur199 <sadiqurrhaman199@gmail.com>
2024-06-07 02:35:08 +0600

    contact page design

--- src/Component/Contact/Contact.jsx
+++ src/Component/Contact/Contact.jsx
@@ -5,9 +5,16 @@ const Contact = () => {
     <div>
       <div className="contact_container">
         <div className="content">
+          <h2>#Contact Us</h2>
           <div className="form">
             <form action="" method='Post'>
               <input type="text" name='Name' value='' placeholder='Enter Your Full Name'required />
+              <input type="email" name='Email' value='' placeholder='Enter Your Email'required />
+              <input type="text" name='Subject' value='' placeholder='Enter Your Subject'required />
+              <textarea name="Message" id="" value='' placeholder='Enter Your Message'>
+              </textarea>
+              <button type='submit'>submit</button>
+
             </form>
           </div>
         </div>
